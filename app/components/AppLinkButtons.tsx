f755a7c87342ced7220c07acc0d475673f657114
Steven Tam <steven.tam123@gmail.com>
2024-10-19 13:09:38 -0500

    add app store icons

---
+++ app/components/AppLinkButtons.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react'
+import AppStoreIcon from '@/assets/app-store-badge-logo.svg'
+import GooglePlayIcon from '@/assets/google-play-badge-logo.svg'
+
+function AppLinkButtons() {
+    const APKUrl = "https://play.google.com/store/apps/details?id=host";
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default AppLinkButtons
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AppLinkButtons from "./components/AppLinkButtons"
 
 export default function Home() {
   return (
@@ -96,6 +97,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <AppLinkButtons />
     </div>
   );
 }
