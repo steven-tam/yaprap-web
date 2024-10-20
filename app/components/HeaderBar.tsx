98ce8826da204c7dc698c3fd06ea7718eb5b4aad
Steven Tam <steven.tam123@gmail.com>
2024-10-20 12:21:31 -0500

    Add Logo

--- app/components/HeaderBar.tsx
+++ app/components/HeaderBar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeaderBar() {
   return (
     <div>
-      <h1>YapRap</h1>
+      <h1 className='text-6xl font-bold'><span className='text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-yellow-400'>YapRap</span></h1>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import GooglePlayButton from "./components/GooglePlayButton"
 import AppStoreButton from "./components/AppStoreButton";
 import HeaderBar from "./components/HeaderBar";
+import YapRapLogo from "@/assets/yaprapkeet-simple3.png";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="flex-1 items-center justify-items-center">
       <HeaderBar />
+      <Image 
+        src={YapRapLogo}
+        width={200}
+        alt="YapRapLogo"
+      />
       <div className="gap-2">
         <AppStoreButton />
         <GooglePlayButton />
