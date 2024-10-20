c8d6c13a42709418df9058d92f68191433d039f0
Steven Tam <steven.tam123@gmail.com>
2024-10-20 11:23:01 -0500

    add app store buttons

---
+++ app/components/HeaderBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeaderBar() {
+  return (
+    <div>
+      <h1>YapRap</h1>
+    </div>
+  )
+}
+
+export default HeaderBar
---
+++ app/components/Screenshots.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Screenshots() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Screenshots
