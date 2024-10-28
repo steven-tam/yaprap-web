4f5c3da55701fc67c06153ae00c15a1811db20d0
Steven Tam <steven.tam123@gmail.com>
2024-10-28 00:54:32 -0500

    add contacts

--- app/components/howitworks-items/RapImage.tsx
+++ app/components/howitworks-items/RapImage.tsx
@@ -10,7 +10,7 @@ function RapImage() {
         target: ref,
         offset: ["0 1", "1.33 1"] // ["start end", "end end"]
     });
-    const translateXProgress = useTransform(scrollYProgress, [0, 0.6], [50, 0]); // Moves from 50px right to 0
+    const translateXProgress = useTransform(scrollYProgress, [0, 0.6], [70, 0]); // Moves from 70px left to 0
     const opacityProgress = useTransform(scrollYProgress, [0, 0.9], [0, 1]);
 
     return (
--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -5,7 +5,11 @@ function Footer() {
   return (
     <div className="flex flex-col items-center justify-center border-2 p-6 w-full" >
       <Socials />
-      <a target='_blank' href="https://docs.google.com/document/d/e/2PACX-1vTUcEORPQPWaxQ-P0bgnoxVdnO7inyQDeyEs7UiqUyXoYGiTtPXEGSipAvSkIJ58zfVqbtPf4x_e8Gj/pub" className='hover:underline text-gray-400'>Private Policy</a>
+      <div>
+        <a target='_blank' href="https://docs.google.com/document/d/e/2PACX-1vTUcEORPQPWaxQ-P0bgnoxVdnO7inyQDeyEs7UiqUyXoYGiTtPXEGSipAvSkIJ58zfVqbtPf4x_e8Gj/pub" className='hover:underline text-gray-400 mx-5'>Private Policy</a>
+        <a target='_blank' href="mailto:yaprap.llc@gmail.com" className='hover:underline text-gray-400 mx-5'>Contacts</a>
+      </div>
+  
     </div>
   )
 }
