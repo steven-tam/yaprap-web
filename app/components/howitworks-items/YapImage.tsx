6508da0617b0cdccf8b322b593cf92137d920e3b
Steven Tam <steven.tam123@gmail.com>
2024-11-03 08:14:39 -0600

    Replace useScroll with stagger animation

--- app/components/howitworks-items/YapImage.tsx
+++ app/components/howitworks-items/YapImage.tsx
@@ -1,22 +1,30 @@
 'use client'
-import React, { useRef } from 'react'
-import { useScroll, motion, useTransform } from 'framer-motion'
+import React from 'react'
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 import YapMockup from '@/assets/yapMockup.svg'
 
+const fadeInAnimationVariant = {
+  initial: {
+    x: -50, 
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition:{
+      duration: 0.5
+    }
+  }
+}
+
 function YapImage() {
-    const ref = useRef<HTMLDivElement>(null)
-    const {scrollYProgress} = useScroll({
-        target: ref,
-        offset: ["0 1", "1.33 1"] // ["start end", "end end"]
-    });
-    const translateXProgress = useTransform(scrollYProgress, [0, 0.8], [-50, 0]); // Moves from -70px right to 0
-    const opacityProgress = useTransform(scrollYProgress, [0, 0.9], [0, 1]);
 
     return (
       <motion.div 
-        ref={ref} 
-        style={{x:translateXProgress, opacity: opacityProgress }} 
+        variants={fadeInAnimationVariant}
+        initial="initial"
+        whileInView="animate"
         className="flex items-center justify-center w-full h-96 gap-10 " 
         >
         <div className='flex flex-col items-center text-center gap-5'>
