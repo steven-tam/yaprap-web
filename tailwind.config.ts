bb352a2fd3cf793f709909b512a0e02a47407073
Steven Tam <steven.tam123@gmail.com>
2024-11-06 22:46:59 -0600

    add peach bg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         // background: "var(--background)",
         // foreground: "var(--foreground)",
+        peach: "#FCE9BE",
+        strongpeach: "#FECF01"
       },
     },
   },
