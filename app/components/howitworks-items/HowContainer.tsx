cbd5752d53797cc506a5ba54351c052c3f5cb059
Steven Tam <steven.tam123@gmail.com>
2024-11-15 15:04:45 -0600

    lint remove delay

--- app/components/howitworks-items/HowContainer.tsx
+++ app/components/howitworks-items/HowContainer.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { delay, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import Goal from './Goal'
 import SimpleGoal from './SimpleGoal'
 import HowTitle from './HowTitle'
