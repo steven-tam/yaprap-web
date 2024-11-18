3ba9656004985b420e23f415b3df6ba8d4d908fa
Steven Tam <steven.tam123@gmail.com>
2024-11-18 11:17:13 -0600

    add links

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -7,26 +7,26 @@ import Image from 'next/image'
 function Socials() {
   return (
     <div className='flex flex-row text-gray-300'>
-      <a href='' className='flex flex-row items-center justify-center'>
+      <a href='https://www.youtube.com/@YapRapApp' target="_blank" className='flex flex-row items-center justify-center'>
         <Image src={YoutubeIcon} 
             height={30}
             alt="Youtube"
         />
         <p className='hover:underline'>/yaprapapp</p>
       </a>
-      <a href='' className='flex flex-row items-center justify-center'>
+      <a href='https://www.instagram.com/yaprapapp/' target="_blank" className='flex flex-row items-center justify-center'>
         <Image src={InstagramIcon} 
             height={30}
             alt="Instagram"
         />
         <p className='hover:underline'>/yaprapapp</p>
       </a>
-      <a href='' className='flex flex-row items-center justify-center'>
+      <a href='https://www.tiktok.com/@yaprapapp?lang=en' target="_blank" className='flex flex-row items-center justify-center'>
         <Image src={TikTokIcon} 
             height={30}
             alt="TikTok"
         />
-        <p className='hover:underline'>/lil_yappa</p>
+        <p className='hover:underline'>/yaprapapp</p>
       </a>
     </div>
   )
