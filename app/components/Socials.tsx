a5f882798d2925c24e9699b3e1fdc88ffdec8092
Steven Tam <60487779+steven-tam@users.noreply.github.com>
2025-02-19 07:42:13 -0600

    made social links adaptable

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -8,26 +8,26 @@ import Image from 'next/image'
 function Socials() {
   return (
     <div className='flex flex-row text-gray-600'>
-      <a href='https://www.youtube.com/@YapRapApp' target="_blank" className='flex flex-row items-center justify-center'>
-        <Image src={YoutubeIcon} 
-            height={30}
-            alt="Youtube"
-        />
-        <p className='hover:underline'>/yaprapapp</p>
-      </a>
       <a href='https://www.instagram.com/yaprapapp/' target="_blank" className='flex flex-row items-center justify-center'>
         <Image src={InstagramIcon} 
             height={30}
             alt="Instagram"
         />
-        <p className='hover:underline'>/yaprapapp</p>
+        <p className='hover:underline hidden md:block'>/yaprapapp</p>
+      </a>
+      <a href='https://www.youtube.com/@YapRapApp' target="_blank" className='flex flex-row items-center justify-center'>
+        <Image src={YoutubeIcon} 
+            height={30}
+            alt="Youtube"
+        />
+        <p className='hover:underline hidden md:block'>/yaprapapp</p>
       </a>
       <a href='https://www.tiktok.com/@yaprapapp?lang=en' target="_blank" className='flex flex-row items-center justify-center'>
         <Image src={TikTokIcon} 
             height={30}
             alt="TikTok"
         />
-        <p className='hover:underline'>/yaprapapp</p>
+        <p className='hover:underline hidden md:block'>/yaprapapp</p>
       </a>
       
       <a href='https://x.com/yaprapapp' target="_blank" className='flex flex-row items-center justify-center ml-1'>
@@ -35,7 +35,7 @@ function Socials() {
             height={16}
             alt="X"
         />
-        <p className='hover:underline'>/yaprapapp</p>
+        <p className='hover:underline hidden md:block'>/yaprapapp</p>
       </a>
     </div>
   )
